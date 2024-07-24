2054e8577a15894c70f47b35e109f67357a20336
Анатолий <lan2002@yandex.ru>
2024-07-24 07:18:02 +0300

    (update) radix_sort.py

--- radix_sort.py
+++ radix_sort.py
@@ -1,39 +1,46 @@
-# Поразрядная сортировка
-array = [170, 45, 1024, 75, 90, 802, 24, 2, 66, 5578]
+# Поразрядная сортировка.
+# Сортирует числа, обрабатывая их поразрядно, от младшего разряда к старшему
+array = [170, 45, 1024, 75, 90, 802, 5578, 24, 2, 66]
 
 
 def counting_sort(arr, exp):
-    n = len(arr)
-    output = [0] * n
+    arr_len = len(arr)
+    output = [0] * arr_len
     count = [0] * 10
 
-    # Подсчитать количество вхождений каждой цифры в текущем разряде
-    for i in range(n):
+    # Подсчитаем количество вхождений каждой цифры в текущем разряде
+    for i in range(arr_len):
         index = arr[i] // exp
         count[index % 10] += 1
 
-    # Изменить count[i] так, чтобы count[i] содержал позицию этой цифры в output[]
+    # Меняем count[i] так, чтобы count[i] содержал позицию этой цифры в output[]
     for i in range(1, 10):
         count[i] += count[i - 1]
 
-    # Построить output массив
-    i = n - 1
+    # Строим output массив
+    i = arr_len - 1
     while i >= 0:
         index = arr[i] // exp
         output[count[index % 10] - 1] = arr[i]
         count[index % 10] -= 1
         i -= 1
 
-    # Скопировать output массив в arr, чтобы arr теперь содержал отсортированные числа по текущему разряду
-    for i in range(n):
+    # Копируем output массив в arr, чтобы arr теперь содержал отсортированные числа по текущему разряду
+    for i in range(arr_len):
         arr[i] = output[i]
 
+
 def radix_sort(arr):
-    # Найти максимальное число, чтобы узнать количество разрядов
+    # Определяем максимальное число, чтобы узнать количество разрядов
     max1 = max(arr)
 
-    # Выполнить сортировку для каждого разряда
+    # Выполняем сортировку для каждого разряда
     exp = 1
     while max1 // exp > 0:
         counting_sort(arr, exp)
         exp *= 10
+    return arr
+
+
+print(array)
+print(radix_sort(array))
