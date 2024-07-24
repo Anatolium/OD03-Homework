f3342385f6e33ae181706725ca619a913d98a2cc
Анатолий <lan2002@yandex.ru>
2024-07-24 06:06:17 +0300

    (update) insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -6,14 +6,10 @@ def insertion_sort(arr):
         key = arr[i]
         j = i - 1
         while j >= 0 and arr[j] > key:
-            print(f"Меняю значение arr[{j + 1}] ({arr[j + 1]}) на значение arr[{j}] ({arr[j]})")
             arr[j + 1] = arr[j]
-            print(arr)
             j -= 1
-        print(f"Меняю значение arr[{j + 1}] ({arr[j + 1]}) на значение key ({key})")
         arr[j + 1] = key
-        print(arr)
     return arr
 
 print(array)
-print("Результат:", insertion_sort(array))
+print(insertion_sort(array))
