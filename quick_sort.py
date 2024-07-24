0279e83fc9c84bd9b2787fcb976dae521839b831
Анатолий <lan2002@yandex.ru>
2024-07-24 03:02:39 +0300

    (new) quick_sort.py

---
+++ quick_sort.py
@@ -0,0 +1,13 @@
+# Быстрая сортировка
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    # Опорный элемент
+    element = arr[0]
+    left = list(filter(lambda i: i < element, arr))
+    center = [i for i in arr if i == element]
+    right = list(filter(lambda i: i > element, arr))
+
+    return quick_sort(left) + center + quick_sort(right)
+
+print(quick_sort([5, 2, 9, 0, 1, 5, 3]))
