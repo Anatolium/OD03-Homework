9f72a512f39444e806a1830508826700de9e3a72
Анатолий <lan2002@yandex.ru>
2024-07-24 06:36:33 +0300

    (update) gnome_sort.py

--- gnome_sort.py
+++ gnome_sort.py
@@ -4,11 +4,13 @@ array = [38, 27, 43, 3, 58, 9, 82, 10, 68]
 
 def gnome_sort(arr):
     index = 0
-    n = len(arr)
-    while index < n:
+    arr_len = len(arr)
+    while index < arr_len:
+        # Если текущий элемент больше или равен предыдущему, переходим к следующему
         if index == 0 or arr[index] >= arr[index - 1]:
             index += 1
         else:
+            # Если текущий элемент меньше предыдущего, меняем их местами и возвращаемся на шаг назад
             arr[index], arr[index - 1] = arr[index - 1], arr[index]
             index -= 1
     return arr
